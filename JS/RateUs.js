060640030e1cda93b59a18d22b5e22cc6bcb7384
BandersGit <andy.i.flaskan@gmail.com>
2020-02-14 09:30:47 +0100

    Survey

--- JS/RateUs.js
+++ JS/RateUs.js
@@ -1,1 +1,55 @@
 const q1answer = document.querySelectorAll("input[name='answer1']");
+
+
+
+const resultButton = document.querySelector(".resultButton");
+resultButton.addEventListener("click", checkResult);
+
+const result = document.querySelector(".result");
+
+let character = ""
+
+function checkResult()
+{
+    let pointsTotal = 0;
+    pointsTotal = pointsTotal + checkAnswer(q1answer);
+    console.log(pointsTotal)
+    
+    if (pointsTotal = 1) 
+    {
+       character = "Darth Vader"; 
+    }
+
+    else if (pointsTotal = 2) 
+    {
+        character = "Kylo Ren";
+    }
+
+    else if (pointsTotal = 3)
+    {
+        character = "Darth Sidius";
+    }
+
+    result.textContent = character;
+}
+
+
+function checkAnswer (q)
+{
+    let pointsTotal = 0;
+
+    for (let index = 0; index < q.length; index++) 
+    {
+        let inputElement = q[index];
+        
+        if (inputElement.checked) 
+        {
+            pointsTotal = pointsTotal + parseInt(inputElement.value)
+        }
+
+    }
+
+    return pointsTotal;
+}
+
+//Fixa poängen
